ff41df52fd9402b031d7d1e25fb8abd18fd22aed
sreeraj <sreerajavk123@gmail.com>
2019-09-01 18:36:32 +0530

    bug fix

--- herbarium/settings.py
+++ herbarium/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'herbarium.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-#
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
