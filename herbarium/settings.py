da854f6f73f24181bb01d72c2e28f0b279f9d7b5
sreeraj <sreerajavk123@gmail.com>
2019-09-01 18:19:58 +0530

    bug fix

--- herbarium/settings.py
+++ herbarium/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'herbarium.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'herbarium',
-#         'USER' : 'sree',
-#         'PASSWORD' : 'password',
-#         'HOST' : 'localhost',
-#         'PORT' : 3306
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'herbarium',
+        'USER' : 'sree',
+        'PASSWORD' : 'password',
+        'HOST' : 'localhost',
+        'PORT' : 3306
+    }
+}
 
 
 # Password validation
